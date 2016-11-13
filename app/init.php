3db8880d1530f5eb51490d3109cc7c61c87b843c
Mo'men Basel <momenbasel@users.noreply.github.com>
2016-11-13 22:17:36 +0200

    Update init.php

--- app/init.php
+++ app/init.php
@@ -4,9 +4,9 @@ error_reporting(0);
 session_start();
 require_once 'vendor/autoload.php';
 
-$fb = new FacebookFacebook(["app_id" => '985408628231361', 'app_secret' => '989309be8fab8d806383c0967d50337c', 'default_graph_version' => 'v2.8'
+$fb = new FacebookFacebook(["app_id" => '(your_app_id)', 'app_secret' => '(your_App_secret)', 'default_graph_version' => 'v2.8'
 
-// this App is on development mode
+
 
 ]);
 $callback = 'http://localhost/facebookAuth/index.php';
